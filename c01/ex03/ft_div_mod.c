e8fab31f49dabff29adaed83430c8802990accf9
Tamires Braga <tamirescamposbraga@gmail.com>
2023-03-18 01:58:03 +0100

    testando main

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -1,5 +1,22 @@
+//#include <stdio.h>
+
 void ft_div_mod(int a, int b, int *div, int *mod)
 {
 	*div = a / b;
 	*mod = a % b;
 }
+
+/*int main(void)
+{
+	int a;
+	int b;
+	int *div;
+	int *mod;
+	a = 3;
+	b = 6;
+	div = &a;
+	mod = &b;
+
+	ft_div_mod(a, b, div, mod);
+	printf("%d %d %d %d\n", a, b, *div, *mod);
+}*/
