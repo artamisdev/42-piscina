70b68a7b9f77c77656b4205f18cab220fa4ccb2f
Tamires Braga <tamirescamposbraga@gmail.com>
2023-03-21 19:30:34 +0100

    ex05 c01

---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+	i = 0;
+	while(str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int main(void)
+{
+	ft_putstr("This is the way!");
+}
