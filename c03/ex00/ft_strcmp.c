8714e59bc85eb9a6ed90d6b09f148bfd3bffbaf8
Tamires <83852578+artamisdev@users.noreply.github.com>
2023-06-12 19:26:51 +0200

    Add files via upload

---
+++ c03/ex00/ft_strcmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tacampos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 15:36:08 by tacampos          #+#    #+#             */
+/*   Updated: 2023/06/12 16:06:05 by tacampos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+/*#include <stdio.h>
+
+int	main(void)
+{
+	printf("Si el valor es positivo la primer string es mas grande.\n");
+	printf("Si el valor es negativo la primer string es mas pequeña.\n");
+	printf("Si el valor es zero, las strings son iguales.\n");
+	printf("\n");
+	printf("Hello / Hello: %d\n", ft_strcmp("Hello", "Hello"));
+	printf("Hello! / Hello: %d\n", ft_strcmp("Hello!", "Hello"));
+	printf("Hello / Hello!: %d\n", ft_strcmp("Hello", "Hello!"));
+	printf("He / Hello: %d\n", ft_strcmp("He", "Hello"));
+	printf("Hello / He: %d\n", ft_strcmp("Hello", "He"));
+}*/
---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tacampos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 18:57:08 by tacampos          #+#    #+#             */
+/*   Updated: 2023/06/12 19:20:27 by tacampos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (j < nb && src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+/*#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char	src0[] = " junto con src.";
+	char	dest0[30] = "Dest,";
+	char	dest1[30] = "Dest,";
+
+	printf("Result STRNCAT: %s\n", strncat(dest0, src0, 15));
+	printf("Result FT_STRNCAT: %s\n", ft_strncat(dest1, src0, 15));
+	return (0);
+}*/
